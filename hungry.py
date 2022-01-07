5a0070a090b0b2a785db7faa723435b3608cc3b8
wubin28 <wubinben@icloud.com>
2022-01-07 09:19:18 +0800

    dumplings

--- hungry.py
+++ hungry.py
@@ -4,5 +4,6 @@ if hungry=="yes" and hungry=="Y" and hungry=="y":
     print("eat pizza")
     print("eat burger")
     print("eat baozi")
+    print("eat dumplings")
 else:
     print("do homework")    
